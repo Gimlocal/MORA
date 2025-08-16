d08d36a866c95fdb0aa12f85f26012ddba35950f
191012 <kimhj191012@naver.com>
2025-08-16 14:54:11 +0900

    feat : UIManager Prefab, DontDestroyOnLoad

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -5,8 +5,20 @@ namespace UI
 {
     public class UIManager : MonoBehaviour
     {
+        public static UIManager Instance { get; private set; }
         private bool _inventoryOpened;
         [SerializeField] private GameObject inventoryPanel;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
 
         private void Update()
         {
