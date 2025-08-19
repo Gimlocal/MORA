8b320f08636f24b98d0df7abf23a0bc8e09eddd7
191012 <kimhj191012@naver.com>
2025-08-19 13:56:55 +0900

    feat : Portal Info Text

--- Assets/Scripts/Object/PortalObject.cs
+++ Assets/Scripts/Object/PortalObject.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,12 +9,16 @@ namespace Object
     {
         [SerializeField] private PortalID portalID;
         [SerializeField] private PortalDatabase portalDatabase;
+        [SerializeField] private Canvas canvas;
+        [SerializeField] private TextMeshProUGUI tmPro;
+        [SerializeField] private String text;
         private PortalData _portalData;
         private bool _isPlayerInRange;
 
         private void Awake()
         {
             _portalData = portalDatabase.GetPortal(portalID);
+            tmPro.text = text;
         }
 
         private void Update()
@@ -30,12 +35,14 @@ namespace Object
         {
             _isPlayerInRange = true;
             Player.Player.Instance.playerAction.canMine = false;
+            canvas.gameObject.SetActive(true);
         }
 
         private void OnTriggerExit2D(Collider2D other)
         {
             _isPlayerInRange = false;
             Player.Player.Instance.playerAction.canMine = true;
+            canvas.gameObject.SetActive(false);
         }
         
         private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
