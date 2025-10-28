790f80addb3a3d5e02948a4510bd5a64ed890aa2
191012 <kimhj191012@naver.com>
2025-10-28 17:29:19 +0900

    feat : Normal, Special, Trap 방 조정
    Normal : 황금 버섯 방 추가
    Special : Collider 오류 수정
    Trap : 가스 방 추가

--- Assets/Scripts/Stage/GasTrapRoom.cs
+++ Assets/Scripts/Stage/GasTrapRoom.cs
@@ -1,12 +1,49 @@
+using System;
+using System.Collections;
+using Player;
 using UnityEngine;
 
 namespace Stage
 {
     public class GasTrapRoom : MonoBehaviour
     {
+        [SerializeField] private float damageDelay;
+        [SerializeField] private float damage;
+        private Coroutine _gasTrapCoroutine;
+        private PlayerStat _stat;
+
+        private void Start()
+        {
+            _stat = Player.Player.Instance.playerStat;
+        }
+
         private void OnTriggerEnter2D(Collider2D other)
         {
-            
+            if (other.CompareTag("Player"))
+            {
+                _stat.isInGasTrap = true;
+                _stat.ApplyPenalty();
+                _gasTrapCoroutine = StartCoroutine(GasTrapCoroutine());
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _stat.isInGasTrap = false;
+                _stat.CheckCorruptionState();
+                StopCoroutine(_gasTrapCoroutine);
+            }
+        }
+
+        private IEnumerator GasTrapCoroutine()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(damageDelay);
+                _stat.Corrupt(damage);
+            }
         }
     }
 }
