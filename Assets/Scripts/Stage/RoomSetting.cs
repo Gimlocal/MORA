d454fef8619b7951fdb6c3aacabf5559dffe6ef8
Gimlocal <127363458+Gimlocal@users.noreply.github.com>
2025-11-03 13:56:02 +0900

    feat : 방 이동 시 이동한 방향으로 미세 이동 추가

--- Assets/Scripts/Stage/RoomSetting.cs
+++ Assets/Scripts/Stage/RoomSetting.cs
@@ -1,11 +1,13 @@
 using System;
 using Cam;
+using UI;
 using UnityEngine;
 
 namespace Stage
 {
     public class RoomSetting : MonoBehaviour
     {
+        public bool setPlayer = true;
         private ParticleSystem _particle;
         private CameraChange _cameraChange;
 
@@ -19,8 +21,13 @@ namespace Stage
         {
             if (other.CompareTag("Player"))
             {
+                SetPlayerPosition(other.transform);
                 _cameraChange.ChangeCamera();
                 _particle?.Play();
+                if (!setPlayer)
+                {
+                    setPlayer = true;
+                }
             }
         }
 
@@ -30,6 +37,21 @@ namespace Stage
             {
                 _particle?.Stop();
             }
+        }
+
+        private void SetPlayerPosition(Transform player)
+        {
+            if (!setPlayer) return;
+            
+            Vector3 playerPos = player.position;
+            Vector3 center = transform.position;
+            Vector3 diff = center - playerPos;
+
+            Vector2 dir = Mathf.Abs(diff.x) > Mathf.Abs(diff.y) ?
+                new Vector3(Mathf.Sign(diff.x), 0) :
+                new Vector3(0, Mathf.Sign(diff.y));
+
+            player.position += (Vector3)dir;
         }
     }
 }
